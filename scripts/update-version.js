fb0a7a57e555c8c6b477c4472bb7e093824e8d5f
Jack Le Beignet <195756165+jacklebeignet@users.noreply.github.com>
2026-08-27 14:43:33 +0200

    chore: format

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -2,20 +2,23 @@ import fs from "fs";
 const version = process.argv[2];
 
 if (!version) {
-    console.error('Usage: node update-version.js <version>');
-    process.exit(1);
+  console.error("Usage: node update-version.js <version>");
+  process.exit(1);
 }
 
-const file = 'version';
-const data = JSON.parse(fs.readFileSync(file, 'utf8'));
+const file = "version";
+const data = JSON.parse(fs.readFileSync(file, "utf8"));
 data.Version = version;
 // Change the version in the version file
-fs.writeFileSync(file, JSON.stringify(data, null, '\t') + '\n');
+fs.writeFileSync(file, JSON.stringify(data, null, "\t") + "\n");
 
 // Change the version in the source code
-const sourceFile = 'source';
-let sourceData = fs.readFileSync(sourceFile, 'utf8');
-sourceData = sourceData.replace(/currentVersion = "[^"]*"/, `currentVersion = "${version}"`);
+const sourceFile = "source";
+let sourceData = fs.readFileSync(sourceFile, "utf8");
+sourceData = sourceData.replace(
+  /currentVersion = "[^"]*"/,
+  `currentVersion = "${version}"`,
+);
 fs.writeFileSync(sourceFile, sourceData);
 
 console.log(`Version updated to ${version}`);
