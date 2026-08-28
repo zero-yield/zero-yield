d7c0261163d9c01e9e080135199b42fd79ab3fa3
Jack Le Beignet <195756165+jacklebeignet@users.noreply.github.com>
2026-08-28 14:11:47 +0200

    Reapply "Merge branch 'organize-code' into next"
    This reverts commit cc4533d8a20ae705f2b5cc3ba6dd113d1ddc5ba7.

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -13,7 +13,7 @@ data.Version = version;
 fs.writeFileSync(file, JSON.stringify(data, null, "\t") + "\n");
 
 // Change the version in the source code
-const sourceFile = "source";
+const sourceFile = "src/core/services.luau";
 let sourceData = fs.readFileSync(sourceFile, "utf8");
 sourceData = sourceData.replace(
   /currentVersion = "[^"]*"/,
